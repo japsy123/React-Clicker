40cbb43b273c4b8afcc0d19714e790b327e019f5
Jal Panchal <jal08panchal@gmail.com>
2019-05-17 19:29:22 -0700

    Convert App to Class Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header" />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header" />
+      </div>
+    );
+  }
 }
 
 export default App;
